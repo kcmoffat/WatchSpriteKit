f13f722940c45eb4df82c548898a012b0bcaf2e3
KASEY MOFFAT <kaseymoffat@kaseys-macbook-pro.local>
2016-04-08 00:01:12 -0700

    Initial Commit

---
+++ WatchSpriteKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchSpriteKit
+//
+//  Created by KASEY MOFFAT on 4/8/16.
+//  Copyright © 2016 KASEY MOFFAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatchSpriteKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchSpriteKit
+//
+//  Created by KASEY MOFFAT on 4/8/16.
+//  Copyright © 2016 KASEY MOFFAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
